48ef3fb9b72c20d146bc842d356530c16ded4705
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-04-08 15:36:20 +0600

    Add files via upload

---
+++ syntax.php
@@ -0,0 +1,134 @@
+<?php
+//    echo "<hmtl>";
+//        echo "<head>";
+//            echo "<title>";
+//                echo "HOME";
+//            echo "</title>";
+//        echo "</head>";
+//        echo "<body>";
+//            echo "<h1>This is a heading.</h1>";
+//            echo "<p>This is a paragraph.</p>";
+//        echo "</body>";
+//    echo "</hmtl>";
+?>
+<!---->
+<!--<html>-->
+<!--    <head>-->
+<!--        <title>--><?php //echo "HOME"; ?><!--</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <h1>--><?php //echo "This is a heading."; ?><!--</h1>-->
+<!--        <p>--><?php //echo "This is a paragraph."; ?><!--</p>-->
+<!---->
+<!--        <input type="text" value="--><?php //echo "Hello World"; ?><!--"/>-->
+<!---->
+<!--    </body>-->
+<!--</html>-->
+<?php
+
+
+    // 3 major rules for creating a variable
+            /*
+                *Start with $ sign
+                *A-Z, a-z, 0-9, _
+                *No number in first
+             */
+    // 3 standard rules for creating a variable
+            /*
+                *small letter
+                *meaning full
+                *readable
+             */
+//        $NAME;
+//        $name;
+//        $NaMe;
+//        $_9name;
+//        $name9;
+//
+////        $number=100;
+//          $firstName = "Rokib";
+//          $lastName = "Hossain";
+//          echo $firstName.' '.$lastName;
+
+//            $name = 132;
+
+
+    /*
+        *Arithmatic Operator
+            *Binary ( +, -, *, /, %)
+            *unary (++, --, (-) )
+        *Assignment Operator
+             =, +=, -=, *=, /=, %=, .=
+        *Conditional Operator
+            >, >=, <, <=, ==, !=, ===, !==
+        *Logical Operator
+            &&, ||, !
+    */
+//        $x=10;
+//        $y=20;
+//        echo $x.=$y;  // $x= $x.$y
+//        echo ++$x;
+//        echo '<br/>';
+//        echo $x--;
+//        echo '<br/>';
+//        echo --$x;
+
+    //Statement
+        /*
+            *Single Line Statement
+            *Conditional Statement
+                *IF
+                *IF ELSE
+                *IF ELSE IF
+                *SWITCH
+            *Repeated Statement
+                *FOR
+                *WHILE
+                *DO WHILE
+                *FOREACH
+        */
+
+//        $x=100;
+//        $y=20;
+//        if($x>$y) {
+//            $z= $x+$y;
+//            echo $z;
+//        } else {
+//
+//        }
+
+
+        function demo() {
+            echo "Hello World";
+        }
+
+        demo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
