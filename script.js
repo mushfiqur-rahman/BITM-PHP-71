0507d704cbf2e5d24ef3ea3623b5601a16fa4fa1
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-04-08 15:21:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+$('#img1').click(function () {
+    var img1SrcValue=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcValue);
+});
+
+$('#img1').click(function () {
+    var img1SrcValue=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcValue);
+});
+
+$('#img1').click(function () {
+    var img1SrcValue=$(this).attr('src');
+    $('#mainImage').attr('src', img1SrcValue);
+});
+
+
+
+
+// function createHeadingParagraphElement() {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+
+//     newHeadingElement.innerHTML = "This is a heading";
+//     newParagraphElement.innerHTML ="This is a paragraph.";
+
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+
+// }
+
+// document.getElementById('btn').onclick=function (ev) {
+//     createHeadingParagraphElement();
+// };
+
+
+// $('#btn').click(function () {
+//     var firstNameValue=$('#firstName').val();
+//     var lastNameValue=$('#lastName').val();
+//     var fullName = firstNameValue+' '+lastNameValue;
+
+//     $('#fullName').val(fullName);
+
+// });
+
+// $('#firstName').keyup(function () {
+//     var firstNameValue =$('#firstName').val();
+//     $('#firstName').val(firstNameValue);
+// });
+
+// $('#lastName').keyup(function () {
+//     var lastNameValue =$('#lastName').val();
+//     $('#lastName').val(lastNameValue);
+// });
+
+// $("#fullName").blur(function () {
+//     var firstName =$('#firstName').val();
+//     var lastName =$('#lastName').val();
+//     var fullName=firstName+' '+lastName;
+//     $('#fullName').val(fullName);
+// });
